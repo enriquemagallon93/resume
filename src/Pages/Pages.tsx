890aea13b22c0d126f9c7f9f9fd910aa1de114c7
Enrique Magallon <magallon.enrique.93@gmail.com>
2025-12-17 01:07:50 -0600

    Lazy load useLayoutEffect

--- src/Pages/Pages.tsx
+++ src/Pages/Pages.tsx
@@ -1,10 +1,10 @@
-import { ReactNode, useLayoutEffect, useRef, useState } from 'react';
+import { ReactNode, useEffect, useMemo, useRef, useState } from 'react';
 import Page from './Page';
 import Theme from '../themes/Theme';
-import { getCloningFunctions, getPathsToSplit } from '../utils/htmlPaginator';
 
 import * as stylex from '@stylexjs/stylex';
 import useSettings from '../Settings/useSettings';
+import PagesCut from './PagesCut';
 
 const styles = stylex.create({
   noJs: {
@@ -12,10 +12,9 @@ const styles = stylex.create({
   }
 });
 
-const Pages= ({ children }: { children: ReactNode }) => {
-  const [pages, setPages] = useState([] as string[]);
-  const [isReady, setIsReady] = useState(false);
-  const [isServerSide, setIsServerSide] = useState(true);
+const Pages = ({ children }: { children: ReactNode }) => {
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
   const {
     width,
     height,
@@ -25,57 +24,30 @@ const Pages= ({ children }: { children: ReactNode }) => {
 
   const originalPage = useRef<HTMLDivElement | null>(null);
 
-  useLayoutEffect(() => {
-    setIsServerSide(false);
-  },[]);
+  const fullPage = useMemo(() => (
+    <div >
+      <Page style={[isFirstRender && styles.noJs]} ref={originalPage}
+        width={width}
+        height={height}
+        horizontalMargin={horizontalMargin}
+        verticalMargin={verticalMargin}
+      >
+        {children}
+      </Page>
+    </div>
+  ), [children, isFirstRender, width, height, horizontalMargin, verticalMargin]);
 
-  useLayoutEffect(() => {
-    setIsReady(false);
-    setPages([]);
-  }, [children, width, height, horizontalMargin, verticalMargin]);
-
-  useLayoutEffect(() => {
-    if (isReady) return;
-    const fullPage = originalPage.current;
-
-    if (!fullPage) return;
-
-    const delayToCalculatePages = setTimeout(() => {
-      const pathsToSplit = getPathsToSplit(fullPage);
-      const { cloneFromPathToLimit } = getCloningFunctions(fullPage);
-
-      const pageElements = pathsToSplit.map((path, index) => {
-        return cloneFromPathToLimit(path, pathsToSplit[index + 1]).outerHTML;
-      });
-
-      setPages(pageElements);
-      setIsReady(true);
-
-    }, 100);
-
-    return () => {
-      clearTimeout(delayToCalculatePages);
-    };
-  }, [isReady]);
+  useEffect(() => {
+    setIsFirstRender(false);
+  }, []);
 
   return (
     <Theme>
       <main>
-        {isReady ? (
-          pages.map((page, index) => {
-            return <div key={index} dangerouslySetInnerHTML={{ __html: page }} />;
-          })
+        {isFirstRender ? (
+          fullPage
         ) : (
-          <div >
-            <Page style={[ isServerSide && styles.noJs]} ref={originalPage}
-              width={width}
-              height={height}
-              horizontalMargin={horizontalMargin}
-              verticalMargin={verticalMargin}
-            >
-              {children}
-            </Page>
-          </div>
+          <PagesCut fullPageRef={originalPage} fallbackContent={fullPage} />
         )}
       </main>
     </Theme>
