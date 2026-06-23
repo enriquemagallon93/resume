4c733d2729f661354c31a06d5547759aff266248
Enrique Magallon <magallon.enrique.93@gmail.com>
2026-06-23 02:41:43 -0600

    Updates

--- assets/useResume-Bu5RsGzV.js
+++
@@ -1,1 +0,0 @@
-import{r as e,R as s}from"./app-CUx2cwSi.js";const o=()=>e.useContext(s);export{o as u};
---
+++ assets/useResume-N1jQKoJo.js
@@ -0,0 +1,1 @@
+import{r as e,R as s,a as t}from"./app-BswYiQGl.js";const u=()=>e.useContext(s),a=()=>e.useContext(t);export{a,u};
